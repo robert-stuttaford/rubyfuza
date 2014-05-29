d4d160d259e50997c389c5e55660a32e1f9a20e7
Robert Stuttaford <robert@cognician.com>
2014-05-29 09:06:46 +0200

    Upgrade to latest Datomic.

--- project.clj
+++ project.clj
@@ -1,4 +1,3 @@
 (defproject rubyfuza "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [com.amazonaws/aws-java-sdk "1.6.12"]
-                 [com.datomic/datomic-free "0.9.4470"]])
+                 [com.datomic/datomic-free "0.9.4766.11"]])
